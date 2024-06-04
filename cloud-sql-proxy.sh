8d1a19d005ee7d05317566daf13bb4efc7466166
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2024-06-04 22:13:10 +0600

    Add cloud-sql-proxy

---
+++ cloud-sql-proxy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+# Cloud SQL Auth Proxy
+# https://cloud.google.com/sql/docs/mysql/sql-proxy#mac-m1
+(
+  set -x; mkdir -p $HOME/cloud-sql-proxy &&
+  LATEST_VERSION=$(curl -s https://api.github.com/repos/GoogleCloudPlatform/cloud-sql-proxy/releases/latest | jq -r '.tag_name') &&
+  curl -o $HOME/cloud-sql-proxy/cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/$LATEST_VERSION/cloud-sql-proxy.darwin.arm64 &&
+  chmod +x $HOME/cloud-sql-proxy/cloud-sql-proxy
+)
