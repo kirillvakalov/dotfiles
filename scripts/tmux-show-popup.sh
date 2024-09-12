230715abe7a54a4380a4a47a3d0ee1a8a1f84a2e
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2024-09-12 17:02:11 +0600

    tmux: Better popup window

---
+++ scripts/tmux-show-popup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+# Based on https://github.com/willhbr/dotfiles/blob/main/bin/show-tmux-popup.sh
+
+session="$(tmux display -p '_popup_#S')"
+
+if ! tmux has -t "$session" 2> /dev/null; then
+  parent_session="$(tmux display -p '#{session_id}')"
+  session_id="$(tmux new-session -c '#{pane_current_path}' -dP -s "$session" -F '#{session_id}' -e TMUX_PARENT_SESSION="$parent_session")"
+  tmux set-option -s -t "$session_id" status off
+  tmux set-option -s -t "$session_id" prefix None
+  session="$session_id"
+fi
+
+exec tmux attach -t "$session" > /dev/null
