a6ce0469f006e6f45c12fdbcb2c4ecad719d157c
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2024-09-14 23:46:21 +0600

    neovim: Setup lazy-nvim package manager

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -13,7 +13,6 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -21,5 +20,29 @@ vim.opt.wrap = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.undodir = os.getenv('HOME') .. '/.local/state/nvim/undodir'
 vim.opt.undofile = true
+
+-- https://lazy.folke.io/installation
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+  },
+  rocks = { enabled = false },
+})
