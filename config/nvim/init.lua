508e9d3da4300cf9cafee4ee6b899490cd37f5b4
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2024-09-15 22:39:41 +0600

    neovim: Use rose-pine theme 🌹🌲

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -24,15 +24,15 @@ vim.opt.undofile = true
 
 -- https://lazy.folke.io/installation
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -40,9 +40,22 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+require('lazy').setup({
   spec = {
-    -- add your plugins here
+    { 'rose-pine/neovim', name = 'rose-pine' },
+  },
+  install = {
+    colorscheme = { 'rose-pine' },
   },
   rocks = { enabled = false },
 })
+
+require('rose-pine').setup({
+  variant = 'main',
+  styles = {
+    italic = false,
+  },
+})
+
+vim.cmd.colorscheme('rose-pine')
+
