8517bba9b0f175aa68db7029a91fe8bf1e0acb44
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2024-09-20 11:25:23 +0600

    neovim: Setup lsp and autocomplete (WIP)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -44,6 +44,9 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   spec = {
     { 'rose-pine/neovim', name = 'rose-pine' },
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
   },
   install = {
     colorscheme = { 'rose-pine' },
@@ -60,3 +63,60 @@ require('rose-pine').setup({
 
 vim.cmd.colorscheme('rose-pine')
 
+-- https://lsp-zero.netlify.app/docs/getting-started.html#plot-twist
+local lspconfig = require('lspconfig')
+
+lspconfig.util.default_config.capabilities = vim.tbl_deep_extend(
+  'force',
+  lspconfig.util.default_config.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end,
+})
+
+require('lspconfig').ts_ls.setup({})
+
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  snippet = {
+    expand = function(args)
+      vim.snippet.expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- https://lsp-zero.netlify.app/docs/autocomplete.html#regular-tab-complete
+    -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/297c3422c27369409276897410d47e4ccb9a8c81/lua/lsp-zero/cmp-mapping.lua#L23-L41
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      local col = vim.fn.col('.') - 1
+
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        fallback()
+      else
+        cmp.complete()
+      end
+    end, {'i', 's'}),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+  }),
+})
