453d14770157b600402e292e6dcc3ea75d428b72
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2024-09-21 11:04:23 +0600

    neovim: Add LspAttach autocommand

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -56,6 +56,17 @@ require("lazy").setup({
   rocks = { enabled = false }
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = { buffer = event.buf }
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  end,
+})
+
 local capabilities = vim.tbl_deep_extend(
   "force",
   {},
