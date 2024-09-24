8bd7d0a5b67f1d729f9f9a8dbad16ad329752803
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2024-09-24 11:44:54 +0600

    neovim: Setup treesitter-textobjects 🔥

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -76,6 +76,7 @@ require('lazy').setup({
     'nvim-lua/plenary.nvim',
     'nvimtools/none-ls.nvim',
     { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    'nvim-treesitter/nvim-treesitter-textobjects',
     { 'nvim-telescope/telescope.nvim', branch = '0.1.x' },
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     { 'echasnovski/mini.nvim', version = false },
@@ -182,6 +183,18 @@ require('nvim-treesitter.configs').setup({
   auto_install = true,
   highlight = { enable = true },
   indent = { enable = true },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+  },
 })
 
 require('telescope').setup()
