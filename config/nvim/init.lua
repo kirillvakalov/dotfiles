46965de00ef5000325ae3e86bd39e624d907561b
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2024-10-12 19:20:39 +0600

    neovim: Add sidescrolloff = 4

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -9,15 +9,16 @@ vim.opt.listchars:append({ trail = '⋅' })
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes'
+
+vim.opt.wrap = false
 vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 4
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-
-vim.opt.wrap = false
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
