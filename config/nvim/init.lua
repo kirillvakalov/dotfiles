834db46ee5c3cfa06135b9bbeffc48653064b5b3
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2024-11-06 17:05:27 +0600

    neovim: Refactor config ♻️

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -38,9 +38,17 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<cr>')
 vim.keymap.set('n', '[d', vim.diagnostic.goto_next)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_prev)
 
+local toggle_diagnostics = function()
+  local enabled = vim.diagnostic.is_enabled()
+  vim.diagnostic.enable(not enabled)
+end
+
+vim.keymap.set('n', '<leader>ud', toggle_diagnostics)
+
 vim.diagnostic.config({
   float = {
     source = 'always',
+    border = 'single',
   },
 })
 
@@ -71,161 +79,206 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
   spec = {
-    { 'rose-pine/neovim', name = 'rose-pine' },
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'neovim/nvim-lspconfig',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/nvim-cmp',
     'nvim-lua/plenary.nvim',
-    'nvimtools/none-ls.nvim',
-    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    { 'nvim-telescope/telescope.nvim', branch = '0.1.x' },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    { 'echasnovski/mini.nvim', version = false },
+    {
+      'rose-pine/neovim',
+      name = 'rose-pine',
+      config = function()
+        require('rose-pine').setup({
+          styles = {
+            italic = false,
+            transparency = true,
+          },
+        })
+        vim.cmd('colorscheme rose-pine')
+      end,
+    },
+    {
+      'neovim/nvim-lspconfig',
+      dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+      },
+      config = function()
+        vim.api.nvim_create_autocmd('LspAttach', {
+          desc = 'LSP actions',
+          callback = function(event)
+            -- https://neovim.io/doc/user/lsp.html#_lua-module:-vim.lsp.semantic_tokens
+            -- https://github.com/NvChad/NvChad/blob/8d2bb359e47d816e67ff86b5ce2d8f5abfe2b631/lua/nvchad/configs/lspconfig.lua#L31-L36
+            -- https://www.reddit.com/r/neovim/comments/zjqquc/comment/izwahv7/
+            local client = vim.lsp.get_client_by_id(event.data.client_id)
+            client.server_capabilities.semanticTokensProvider = nil
+
+            local opts = { buffer = event.buf }
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+            vim.keymap.set('n', '<leader>f', '<cmd>lua vim.lsp.buf.format({ async = true })<cr>', opts)
+          end,
+        })
+
+        local has_cmp, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+        local capabilities = vim.tbl_deep_extend(
+          'force',
+          {},
+          vim.lsp.protocol.make_client_capabilities(),
+          has_cmp and cmp_nvim_lsp.default_capabilities() or {}
+        )
+
+        require('mason').setup()
+
+        require('mason-lspconfig').setup({
+          ensure_installed = { 'ts_ls', 'biome', 'eslint' },
+          handlers = {
+            function(server_name)
+              require('lspconfig')[server_name].setup({
+                capabilities = capabilities,
+              })
+            end,
+          },
+        })
+      end,
+    },
+    {
+      'williamboman/mason.nvim',
+      config = function()
+        require('mason').setup()
+        -- https://github.com/LazyVim/LazyVim/blob/a1c3ec4cd43fe61e3b614237a46ac92771191c81/lua/lazyvim/plugins/lsp/init.lua#L289-L296
+        local masonRegistry = require('mason-registry')
+
+        masonRegistry.refresh(function()
+          for _, tool in ipairs({ 'stylua', 'prettier' }) do
+            local pkg = masonRegistry.get_package(tool)
+            if not pkg:is_installed() then
+              pkg:install()
+            end
+          end
+        end)
+      end,
+    },
+    {
+      'hrsh7th/nvim-cmp',
+      dependencies = {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+      },
+      config = function()
+        local cmp = require('cmp')
+
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              vim.snippet.expand(args.body)
+            end,
+          },
+          mapping = cmp.mapping.preset.insert({
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v4.x/lua/lsp-zero/cmp-mapping.lua#L28-L41
+            ['<Tab>'] = cmp.mapping(function(fallback)
+              local col = vim.fn.col('.') - 1
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                fallback()
+              else
+                cmp.complete()
+              end
+            end, { 'i', 's' }),
+            ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          }),
+          sources = {
+            { name = 'nvim_lsp' },
+            { name = 'buffer' },
+          },
+        })
+      end,
+    },
+    {
+      'nvimtools/none-ls.nvim',
+      dependencies = { 'williamboman/mason.nvim' },
+      config = function()
+        local null_ls = require('null-ls')
+        null_ls.setup({
+          sources = {
+            null_ls.builtins.formatting.stylua,
+            null_ls.builtins.formatting.prettier,
+          },
+        })
+      end,
+    },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      build = ':TSUpdate',
+      dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+      config = function()
+        require('nvim-treesitter.configs').setup({
+          auto_install = true,
+          highlight = { enable = true },
+          indent = { enable = true },
+          textobjects = {
+            select = {
+              enable = true,
+              lookahead = true,
+              keymaps = {
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+              },
+            },
+          },
+        })
+      end,
+    },
+    {
+      'nvim-telescope/telescope.nvim',
+      dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+        {
+          'nvim-telescope/telescope-fzf-native.nvim',
+          build = 'make',
+          enabled = vim.fn.executable('make') == 1,
+        },
+      },
+      config = function()
+        pcall(require('telescope').load_extension, 'fzf')
+        local actions = require('telescope.actions')
+
+        require('telescope').setup({
+          pickers = {
+            live_grep = {
+              mappings = {
+                i = { ['<C-f>'] = actions.to_fuzzy_refine },
+              },
+            },
+          },
+        })
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files)
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+        vim.keymap.set('n', '<leader>fb', builtin.buffers)
+      end,
+    },
+    {
+      'echasnovski/mini.files',
+      version = '*',
+      config = function()
+        require('mini.files').setup()
+        -- https://github.com/LazyVim/LazyVim/blob/a1c3ec4cd43fe61e3b614237a46ac92771191c81/lua/lazyvim/plugins/extras/editor/mini-files.lua#L16-L22
+        vim.keymap.set('n', '<leader>fm', function()
+          require('mini.files').open(vim.api.nvim_buf_get_name(0), true)
+        end)
+      end,
+    },
     -- Build good vim habits
-    { 'm4xshen/hardtime.nvim', dependencies = { 'MunifTanjim/nui.nvim' }, opts = { disable_mouse = false } },
+    {
+      'm4xshen/hardtime.nvim',
+      dependencies = { 'MunifTanjim/nui.nvim' },
+      opts = { disable_mouse = false },
+    },
     'ThePrimeagen/vim-be-good',
     'github/copilot.vim',
   },
   rocks = { enabled = false },
 })
-
-require('rose-pine').setup({
-  styles = {
-    italic = false,
-    transparency = true,
-  },
-})
-
-vim.cmd('colorscheme rose-pine')
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    -- https://neovim.io/doc/user/lsp.html#_lua-module:-vim.lsp.semantic_tokens
-    -- https://github.com/NvChad/NvChad/blob/8d2bb359e47d816e67ff86b5ce2d8f5abfe2b631/lua/nvchad/configs/lspconfig.lua#L31-L36
-    -- https://www.reddit.com/r/neovim/comments/zjqquc/comment/izwahv7/
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-    client.server_capabilities.semanticTokensProvider = nil
-
-    local opts = { buffer = event.buf }
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>f', '<cmd>lua vim.lsp.buf.format({ async = true })<cr>', opts)
-  end,
-})
-
-local capabilities = vim.tbl_deep_extend(
-  'force',
-  {},
-  vim.lsp.protocol.make_client_capabilities(),
-  require('cmp_nvim_lsp').default_capabilities()
-)
-
-require('mason').setup()
-
--- https://github.com/LazyVim/LazyVim/blob/a1c3ec4cd43fe61e3b614237a46ac92771191c81/lua/lazyvim/plugins/lsp/init.lua#L289-L296
-local masonRegistry = require('mason-registry')
-
-masonRegistry.refresh(function()
-  for _, tool in ipairs({ 'stylua', 'prettier', 'biome' }) do
-    local pkg = masonRegistry.get_package(tool)
-    if not pkg:is_installed() then
-      pkg:install()
-    end
-  end
-end)
-
-require('mason-lspconfig').setup({
-  ensure_installed = { 'ts_ls' },
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({
-        capabilities = capabilities,
-      })
-    end,
-  },
-})
-
-local cmp = require('cmp')
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.snippet.expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v4.x/lua/lsp-zero/cmp-mapping.lua#L28-L41
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      local col = vim.fn.col('.') - 1
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-  },
-})
-
-local null_ls = require('null-ls')
-
-null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.biome,
-  },
-})
-
-require('nvim-treesitter.configs').setup({
-  auto_install = true,
-  highlight = { enable = true },
-  indent = { enable = true },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-  },
-})
-
-require('telescope').setup({
-  pickers = {
-    live_grep = {
-      mappings = {
-        i = { ['<C-f>'] = require('telescope.actions').to_fuzzy_refine },
-      },
-    },
-  },
-})
-require('telescope').load_extension('fzf')
-
-local telescopeBuiltin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescopeBuiltin.find_files)
-vim.keymap.set('n', '<leader>fg', telescopeBuiltin.live_grep)
-vim.keymap.set('n', '<leader>fb', telescopeBuiltin.buffers)
-
-require('mini.files').setup()
--- https://github.com/LazyVim/LazyVim/blob/a1c3ec4cd43fe61e3b614237a46ac92771191c81/lua/lazyvim/plugins/extras/editor/mini-files.lua#L16-L22
-vim.keymap.set('n', '<leader>fm', function()
-  require('mini.files').open(vim.api.nvim_buf_get_name(0), true)
-end)
