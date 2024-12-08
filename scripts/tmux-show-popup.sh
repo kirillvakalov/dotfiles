a9813758bc3948a9a0818eca4d3b8dc551b74e9a
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2024-12-08 10:40:15 +0600

    tmux: Setup resurrect/continuum plugins, update popup script

--- scripts/tmux-show-popup.sh
+++ scripts/tmux-show-popup.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env zsh
 # Based on https://github.com/willhbr/dotfiles/blob/main/bin/show-tmux-popup.sh
 
-session="$(tmux display -p '_popup_#S')"
+session="_popup"
 
 if ! tmux has -t "$session" 2> /dev/null; then
-  parent_session="$(tmux display -p '#{session_id}')"
-  session_id="$(tmux new-session -c '#{pane_current_path}' -dP -s "$session" -F '#{session_id}' -e TMUX_PARENT_SESSION="$parent_session")"
+  session_id="$(tmux new-session -c '#{pane_current_path}' -dP -s "$session" -F '#{session_id}')"
   tmux set-option -s -t "$session_id" status off
   tmux set-option -s -t "$session_id" prefix None
   session="$session_id"
