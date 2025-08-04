b3fd9ebbefb19500c4329e0c73e79112b49e7c07
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2025-08-04 19:37:50 +0600

    Get latest v2 version of cloud-sql-proxy

--- scripts/cloud-sql-proxy.sh
+++ scripts/cloud-sql-proxy.sh
@@ -4,7 +4,7 @@
 # https://cloud.google.com/sql/docs/mysql/sql-proxy#mac-m1
 (
   set -x; mkdir -p $HOME/cloud-sql-proxy &&
-  LATEST_VERSION=$(curl -s https://api.github.com/repos/GoogleCloudPlatform/cloud-sql-proxy/releases/latest | jq -r '.tag_name') &&
+  LATEST_VERSION=$(curl -s https://api.github.com/repos/GoogleCloudPlatform/cloud-sql-proxy/releases | jq -c 'first(.[] | select(.tag_name | startswith("v2")))' | jq -r '.tag_name') &&
   curl -o $HOME/cloud-sql-proxy/cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/$LATEST_VERSION/cloud-sql-proxy.darwin.arm64 &&
   chmod +x $HOME/cloud-sql-proxy/cloud-sql-proxy
 )
