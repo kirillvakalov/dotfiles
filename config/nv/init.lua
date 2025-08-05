d01a2657a50ed2949f596dd2462931297c0365a0
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2025-08-05 18:19:51 +0600

    Update nv config

--- config/nv/init.lua
+++ config/nv/init.lua
@@ -1,5 +1,6 @@
 -- https://neovim.io/doc/user/options.html
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.mouse = 'a'
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -9,7 +10,20 @@ vim.opt.cursorline = true
 vim.opt.cursorlineopt = 'number'
 
 -- https://neovim.io/doc/user/usr_30.html#_tabs-and-spaces
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 2 -- Indent size
 vim.opt.softtabstop = 2
 vim.opt.smartindent = true
+
+vim.opt.ignorecase = true -- Ignore case in search
+vim.opt.smartcase = true -- Don't ignore case with uppercase chars
+
+vim.opt.list = true -- Show invisible chars (tabs, trailing spaces, etc...)
+vim.opt.listchars:append({ trail = '⋅', precedes = '<', extends = '>' })
+vim.opt.wrap = false
+vim.opt.sidescrolloff = 6
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.undofile = true
