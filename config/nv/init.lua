634341637781d97dc2921f63bb0ae1dd4395d95f
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2025-08-05 20:05:43 +0600

    Add mini.deps to nv config

--- config/nv/init.lua
+++ config/nv/init.lua
@@ -1,3 +1,19 @@
+-- https://github.com/echasnovski/mini.deps?tab=readme-ov-file#installation
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.deps'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.deps`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/echasnovski/mini.deps', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.deps | helptags ALL')
+  vim.cmd('echo "Installed `mini.deps`" | redraw')
+end
+
+require('mini.deps').setup({ path = { package = path_package } })
+
 -- https://neovim.io/doc/user/options.html
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.mouse = 'a'
