d16c769c48242f231eeea0a0425d0c3e42811f85
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2025-08-05 23:13:47 +0600

    nv: Setup smart-splits plugin

--- config/nv/init.lua
+++ config/nv/init.lua
@@ -72,3 +72,14 @@ require('oil').setup({
   view_options = { show_hidden = true },
 })
 vim.keymap.set('n', '-', '<cmd>Oil<cr>')
+
+add({ source = 'mrjones2014/smart-splits.nvim' })
+local smart_splits = require('smart-splits')
+vim.keymap.set('n', '<M-Left>', smart_splits.resize_left)
+vim.keymap.set('n', '<M-Down>', smart_splits.resize_down)
+vim.keymap.set('n', '<M-Up>', smart_splits.resize_up)
+vim.keymap.set('n', '<M-Right>', smart_splits.resize_right)
+vim.keymap.set('n', '<C-h>', smart_splits.move_cursor_left)
+vim.keymap.set('n', '<C-j>', smart_splits.move_cursor_down)
+vim.keymap.set('n', '<C-k>', smart_splits.move_cursor_up)
+vim.keymap.set('n', '<C-l>', smart_splits.move_cursor_right)
