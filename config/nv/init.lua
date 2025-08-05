3e610686cfc4698a83572d4e5fd4a26b24b16d29
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2025-08-05 16:45:27 +0600

    Update nv config

--- config/nv/init.lua
+++ config/nv/init.lua
@@ -1,5 +1,15 @@
+-- https://neovim.io/doc/user/options.html
 vim.opt.clipboard = 'unnamedplus'
+
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
+
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = 'number'
+
+-- https://neovim.io/doc/user/usr_30.html#_tabs-and-spaces
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
