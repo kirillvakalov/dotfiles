ca0f9d6fcad2a44feb89ef96eb91c35b5650a9bd
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2026-01-25 11:58:26 +0600

    nvim: Replace arrow with harpoon2

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -285,8 +285,16 @@ require('gitlinker').setup()
 vim.keymap.set({ 'n', 'v' }, '<leader>gy', '<cmd>GitLink<cr>')
 vim.keymap.set({ 'n', 'v' }, '<leader>gY', '<cmd>GitLink!<cr>')
 
-add({ source = 'otavioschwanck/arrow.nvim' })
-require('arrow').setup({
-  show_icons = false,
-  leader_key = ';',
+add({
+  source = 'ThePrimeagen/harpoon',
+  checkout = 'harpoon2',
+  depends = { 'nvim-lua/plenary.nvim' },
 })
+local harpoon = require('harpoon')
+harpoon:setup()
+vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
+vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)
+vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
+vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)
+vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)
